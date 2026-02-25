de6565344d92a303cdd7264029012d633613db2b
TanEd123 <hello@tanyaedgar.com>
2026-02-25 11:24:21 +0000

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Keyword Research Bot",
+  description: "Find keywords your ideal clients are searching for",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
